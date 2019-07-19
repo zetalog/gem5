6c2e2ead26b3d5d82d2f024350d7f20702040b55
IanJiangICT <ianjiang.ict@gmail.com>
2019-07-19 19:48:50 +0800

    Use fix width 64 for Rd of LDRx/STRx (register) disassembly.
    Change-Id: Ia135266389b38fc9f3af66387bffde685b26193a

--- src/arch/arm/insts/mem64.cc
+++ src/arch/arm/insts/mem64.cc
@@ -152,6 +152,37 @@ MemoryPostIndex64::generateDisassembly(Addr pc, const SymbolTable *symtab) const
         ccprintf(ss, "]");
     return ss.str();
 }
+void
+MemoryReg64 ::printExtendOperand(bool firstOperand, std::ostream &os,
+                                  IntRegIndex rm, ArmExtendType type,
+                                  int64_t shiftAmt) const
+{
+    if (!firstOperand)
+        ccprintf(os, ", ");
+    printIntReg(os, rm, 64);
+    if (type == UXTX && shiftAmt == 0)
+        return;
+    switch (type) {
+      case UXTB: ccprintf(os, ", UXTB");
+        break;
+      case UXTH: ccprintf(os, ", UXTH");
+        break;
+      case UXTW: ccprintf(os, ", UXTW");
+        break;
+      case UXTX: ccprintf(os, ", LSL");
+        break;
+      case SXTB: ccprintf(os, ", SXTB");
+        break;
+      case SXTH: ccprintf(os, ", SXTH");
+        break;
+      case SXTW: ccprintf(os, ", SXTW");
+        break;
+      case SXTX: ccprintf(os, ", SXTW");
+        break;
+    }
+    if (type == UXTX || shiftAmt)
+        ccprintf(os, " #%d", shiftAmt);
+}
 
 std::string
 MemoryReg64::generateDisassembly(Addr pc, const SymbolTable *symtab) const
